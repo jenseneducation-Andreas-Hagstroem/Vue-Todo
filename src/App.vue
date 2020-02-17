738d5c7009a72fcc1cd16f80df283714e0baac26
Andreas Hagström <ahagstroem@gmail.com>
2020-02-17 09:32:59 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <Header />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+export default {
+  name: "app",
+  components: {
+    Header,
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "1/ Köpa mjölk",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "2/ Gå ut med hunden",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "3/ Sova",
+          completed: false
+        }
+      ]
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id != id);
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
